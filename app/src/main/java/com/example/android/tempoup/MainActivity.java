f6e2e49052fdbfdb09e56ac03f2b11e46ee8ac30
SkrighYZ <yipengzhang6@yahoo.com>
2018-09-09 14:15:24 -0400

    Test commits using git

--- app/src/main/java/com/example/android/tempoup/MainActivity.java
+++ app/src/main/java/com/example/android/tempoup/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawView drawView;
     TextView tempoTextView;
     CircularSeekBar seekBar;
+    ToggleButton toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +40,18 @@ public class MainActivity extends AppCompatActivity {
         });
         seekBar.setBackgroundColor(getResources().getColor(R.color.colorBackgroundGrey));
         seekBar.setRingBackgroundColor(getResources().getColor(R.color.colorBackgroundWhite));
+
+        toggle = (ToggleButton) findViewById(R.id.toggle);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(), "Toggle is on", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Toggle is off", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
